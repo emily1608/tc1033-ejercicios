ab5d2beb9a91ee56045241b2efacace6207800ae
emily1608 <emilygrettelm@gmail.com>
2019-12-06 23:17:48 -0600

    Implementación del main

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,4 +6,16 @@ def greet():
 def run(self,_numero):
     for x in range(0, _numero):
         print("Running")
-    
+
+if __name__=="__main__":
+    nombre=Nombre()
+    number=Run()
+    nombre.greet("Hansel", "Gretel")
+
+    print("Ingreas un número que se encuentre 0 y 10.")
+    numero = int(input())
+    while numero <0 numero >=10:
+        print("ERROR")
+        print("Ingreas un número nuevo que se encuentre 0 y 10. PORFAVOR")
+        numero = int(input())
+        numero.run(number)
