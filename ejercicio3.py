a3ebca164a8af80079b90b18747758fec120c337
emily1608 <emilygrettelm@gmail.com>
2019-12-07 12:31:18 -0600

    Implementación clases Ruedas

--- ejercicio3.py
+++ ejercicio3.py
@@ -10,12 +10,20 @@ class Maritimos(Vehiculos):
         self.tipo_propulsion = _tipo_propulsion
 
 class Aereos(Vehiculos):
-    def __init__(self):
+    def __init__(self, _tipo_propulsion):
+        self.tipo_propulsion = _tipo_propulsion
 
-class Terrestres(Vehiculos):
+class Terrestres(Vehiculos,Ruedas):
     def __init__(self, _no_ruedas, _datos_motor, _no_puertas):
         self.no_ruedas = _no_ruedas
         self.datos_motor = _datos_motor
         self.no_puertas = _no_puertas
+
+class Ruedas():
+    def __init__(self, _tamaño_in, _presion_ideal, _fabricante):
+        self.tamaño_in = _tamaño_in
+        self.presion_ideal = _presion_ideal
+        self.fabricante = _fabricante
+
 
 
