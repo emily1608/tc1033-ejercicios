de12e56af64eed14af08e5ab21aef538a385052b
emily1608 <emilygrettelm@gmail.com>
2019-12-06 22:36:09 -0600

    Implementación de función run

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,9 @@
 #Morales Hernández Emily Grettel
-def Greet():
+def greet():
     nombre=(input("What is your name?"))
     return nombre
+
+def run(self,_numero):
+    for x in range(0, _numero):
+        print("Running")
+    
