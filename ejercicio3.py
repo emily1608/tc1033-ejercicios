b6dd03cbe68699724b5fb9b06741eeb066cb21d1
emily1608 <emilygrettelm@gmail.com>
2019-12-07 12:23:31 -0600

    Implementación clase Maritimos

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,3 +1,13 @@
 class Vehiculos():
     def __init__(self, _matricula, _medio, _cap_pasajeros, _vel_max):
+        self.matricula = _matricula
+        self.medio = _medio
+        self.cap_pasajeros = _cap_pasajeros
+        self.vel_max = _vel_max
+
+class Maritimos(Vehiculos):
+    def __init__(self, _tipo_propulsion):
+        self.tipo_propulsion = _tipo_propulsion
+
+
 
